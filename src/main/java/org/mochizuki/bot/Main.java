669f0491276d348ba115c2d84efc94808ae3ed9a
YukinaMochizuki <1p41p4jejo@gmail.com>
2017-12-15 22:54:50 +0800

    Update build number

--- src/main/java/org/mochizuki/bot/Main.java
+++ src/main/java/org/mochizuki/bot/Main.java
@@ -24,7 +24,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        System.err.print("System build 0.1214");
+        System.err.print("System build 0.1215");
         logger.info("Starting L2 cache support system");
         logger.info("version " + version);
 
