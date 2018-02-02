bbfb537abe784c9e4c43f979c42307778015e47a
mochizuki <1p41p4jejo@gmail.com>
2018-02-02 19:43:12 +0800

    Update interface

--- src/main/java/org/mochizuki/bot/service/ServiceInterface.java
+++ src/main/java/org/mochizuki/bot/service/ServiceInterface.java
@@ -1,8 +1,12 @@
 package org.mochizuki.bot.service;
 
+import org.mochizuki.bot.service.unit.ServiceContainer;
+
 import java.util.logging.Logger;
 
 public interface ServiceInterface {
     void displayMessage(Logger logger, String message);
     void displayWarnMessage(Logger logger,String message);
+    void setProvider(Object plugin, Class serviceClass, Object provider);
+    ServiceContainer provide(Class serviceClass);
 }
