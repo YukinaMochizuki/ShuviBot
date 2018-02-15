32743e5203091d4eba29f8bd4fef0eda222dcd9e
YukinaMochizuki <1p41p4jejo@gmail.com>
2018-02-15 14:44:25 +0800

    bug fix and update .jar

--- src/main/java/org/mochizuki/bot/unit/Enum/BotState.java
+++
@@ -1,9 +0,0 @@
-package org.mochizuki.bot.unit.Enum;
-
-@Deprecated
-public enum BotState {
-
-    CONSTRUCTION,PRE_INITIALIZATION,INITIALIZATION,POST_INITIALIZATION,
-
-    LOAD_COMPLETE,SERVER_STOPPING,SERVER_STOPPED,
-}
